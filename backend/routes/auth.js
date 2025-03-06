a5e60b704e463cd21376b72cdc9d7b9483578b2d
Canberkkmn <canberkkaman@gmail.com>
2025-03-07 01:57:37 +0300

    Add XSS protection

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,44 +2,76 @@ const express = require("express");
 const User = require("../models/User.js");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const { body, validationResult } = require("express-validator");
+const sanitizeHtml = require("sanitize-html");
 
 const router = express.Router();
 
-// Kullanıcı kaydı
-router.post("/register", async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
-    const userExists = await User.findOne({ email });
+router.post(
+  "/register",
+  [
+    body("username").trim().escape(),
+    body("email").isEmail().normalizeEmail(),
+    body("password").isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
 
-    if (userExists)
-      return res.status(400).json({ message: "User already exists" });
-
-    const user = await User.create({ username, email, password });
-
-    res.status(201).json({ message: "User created successfully!" });
-  } catch (error) {
-    res.status(500).json({ message: "Server error" });
-  }
-});
-
-// Kullanıcı giriş
-router.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-
-    if (!user || !(await bcrypt.compare(password, user.password))) {
-      return res.status(401).json({ message: "Invalid credentials" });
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-    });
+    req.body.username = sanitizeHtml(req.body.username);
+    req.body.email = sanitizeHtml(req.body.email);
 
-    res.json({ token });
-  } catch (error) {
-    res.status(500).json({ message: "Server error" });
+    try {
+      const { username, email, password } = req.body;
+      const userExists = await User.findOne({ email });
+
+      if (userExists)
+        return res.status(400).json({ message: "User already exists" });
+
+      const user = await User.create({ username, email, password });
+
+      res.status(201).json({ message: "User created successfully!" });
+    } catch (error) {
+      res.status(500).json({ message: "Server error" });
+    }
   }
-});
+);
+
+router.post(
+  "/login",
+  [
+    body("email").isEmail().normalizeEmail(),
+    body("password").isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    req.body.email = sanitizeHtml(req.body.email);
+
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
+
+      if (!user || !(await bcrypt.compare(password, user.password))) {
+        return res.status(401).json({ message: "Invalid credentials" });
+      }
+
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+      });
+
+      res.json({ token });
+    } catch (error) {
+      res.status(500).json({ message: "Server error" });
+    }
+  }
+);
 
 module.exports = router;
